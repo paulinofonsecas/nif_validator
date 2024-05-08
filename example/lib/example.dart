d9ed386d89aa4cd9f8a878cc79c8b2242b4f5791
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-05-08 13:14:56 +0100

    upgrade do pacote no projeto de exemplo

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -3,13 +3,13 @@ import 'package:nif_validator/nif_validator.dart';
 void main() async {
   final validator = NIFValidator();
 
-  final nif = '005489315Be041';
+  final nif = '005181092BE045';
   final result = await validator.validate(nif);
 
   if (result.isValid) {
     print('NIF $nif é válido.');
     print('Nome: ${(result as NIFValidatorResponse).name}.');
   } else {
-    print('NIF $nif é inválido: ${(result as NIFValidatorError).message}');
+    print('MSG: ${(result as NIFValidatorError).message}');
   }
 }
